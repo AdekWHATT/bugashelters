b44ceeae58da60e5994842e80025c5a309a7e17e
Vyacheslaw Kilin <vyacheslaw.kilin@mail.ru>
2023-03-18 23:33:50 +0300

    stable

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Shelter from './components/Shelter/Shelter';
 import News from './components/News/News';
+import Dashboard from './components/Dashboard/Dashboard'
 // import ShelterTest from './components/Shelertest/ShelterTest';
 function App() {
   return (
     <>
-      <Shelter/>
-      <News/>
+      <Shelter />
+      <News />
+      <Dashboard />
       {/* <ShelterTest /> */}
     </>
   );
--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -1,67 +1,88 @@
 import React, { useState, useEffect } from 'react';
-import NewsAPI from 'newsapi';
-
-const newsapi = new NewsAPI('a63ddc24567546db8b9c3141919af3ee');
+import './News.css';
+import Dashboard from '../Dashboard/Dashboard';
 
 const News = () => {
-  const [articles, setArticles] = useState([]);
-  const [filteredArticles, setFilteredArticles] = useState([]);
-  const [selectedCategory, setSelectedCategory] = useState('all');
+    const [articles, setArticles] = useState([]);
+    const [filteredArticles, setFilteredArticles] = useState([]);
+    const [selectedCategory, setSelectedCategory] = useState('all');
 
-  useEffect(() => {
-    newsapi.v2.topHeadlines({
-      country: 'ru'
-    }).then(response => {
-      setArticles(response.articles);
-      setFilteredArticles(response.articles);
-    }).catch(error => console.error(error));
-  }, []);
+    useEffect(() => {
+        const apiUrl = selectedCategory === 'all'
+            ? 'https://newsapi.org/v2/top-headlines?country=ru&apiKey=a63ddc24567546db8b9c3141919af3ee'
+            : `https://newsapi.org/v2/top-headlines?country=ru&category=${selectedCategory}&apiKey=a63ddc24567546db8b9c3141919af3ee`;
 
-  const categories = ['all', 'business', 'entertainment', 'health', 'science', 'sports', 'technology'];
+        fetch(apiUrl)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                setArticles(data.articles);
+                setFilteredArticles(data.articles);
+            })
+            .catch(error => console.error(error));
+    }, [selectedCategory]);
 
-  const handleCategoryChange = (category) => {
-    setSelectedCategory(category);
+    const categories = [
+        { name: 'Все новости', value: 'all' },
+        { name: 'Бизнеса', value: 'business' },
+        { name: 'Развлечения', value: 'entertainment' },
+        { name: 'Здоровья', value: 'health' },
+        { name: 'Науки', value: 'science' },
+        { name: 'Спорта', value: 'sports' },
+        { name: 'Технологии', value: 'technology' },
+    ];
 
-    if (category === 'all') {
-      setFilteredArticles(articles);
-    } else {
-      const filtered = articles.filter(article => article.category === category);
-      setFilteredArticles(filtered);
-    }
-  };
+    const handleCategoryChange = (selectedCategory) => {
+        setSelectedCategory(selectedCategory);
 
-  return (
-    <>
-      <h1 className='p-3'>News</h1>
+        if (selectedCategory === 'all') {
+            setFilteredArticles(articles);
+        } else {
+            fetch(`https://newsapi.org/v2/top-headlines?country=ru&category=${selectedCategory}&apiKey=a63ddc24567546db8b9c3141919af3ee`)
+                .then(response => response.json())
+                .then(data => {
+                    setFilteredArticles(data.articles);
+                })
+                .catch(error => console.error(error));
+        }
+    };
 
-      <div className="filter">
-        {categories.map(category => (
-          <button
-            key={category}
-            className={selectedCategory === category ? 'active' : ''}
-            onClick={() => handleCategoryChange(category)}
-          >
-            {category}
-          </button>
-        ))}
-      </div>
+    return (
+        <>
+            <h1 className='p-3'>Новости {categories.name}</h1>
+<Dashboard categories={categories}/>
+            {/* <div className="filter_container">
+                {categories.map(category => (
+                    <button
+                        key={category.value}
+                        className={selectedCategory === category.value ? 'filter-link active' : 'filter-link'}
+                        onClick={() => handleCategoryChange(category.value)}
+                    >
+                        {category.name}
+                    </button>
+                ))}
+            </div> */}
 
-      <ul className='news-cards-list'>
-        {filteredArticles.map(article => (
-          <li key={article.url} className='news-card'>
-            <div className='news-card-image'>
-              {article.urlToImage && <img src={article.urlToImage} alt={article.title} />}
-            </div>
-            <div className='news-card-content'>
-              <h2>{article.title}</h2>
-              <p>{article.description}</p>
-              <a href={article.url} target='_blank' rel='noopener noreferrer'>Read more</a>
-            </div>
-          </li>
-        ))}
-      </ul>
-    </>
-  );
+            <ul className='news-cards-list'>
+                {filteredArticles.map(article => (
+                    <li key={article.url} className='news-card'>
+                        <div className='news-card-image'>
+                            {article.urlToImage ? (
+                                <img width={400} height={300} src={article.urlToImage} alt={article.title} />
+                            ) : (
+                                <p>Источник: {article.author}</p>
+                            )}
+                        </div>
+                        <div className='news-card-content'>
+                            <h2>{article.title}</h2>
+                            <p>{article.description}</p>
+                            <a href={article.url} target='_blank' rel='noopener noreferrer'>Читать далее...</a>
+                        </div>
+                    </li>
+                ))}
+            </ul>
+        </>
+    );
 }
 
 export default News;
