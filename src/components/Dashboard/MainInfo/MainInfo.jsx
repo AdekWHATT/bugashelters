a274526933f8c1fbdce90f2030ab927cab6181c4
Vyacheslaw Kilin <vyacheslaw.kilin@mail.ru>
2023-03-19 19:15:07 +0300

    stable 777

--- src/components/Dashboard/MainInfo/MainInfo.jsx
+++ src/components/Dashboard/MainInfo/MainInfo.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
-
+import mainLeftBanner from '../../../img/1583429434.jpg'
 const MainInfo = () => {
   return (
-    <div className='row p-3'>
+    <div className='row p-4'>
        <div className='col-6'>
     <div className='left_main__banner'>
-      <img src="./img/1583429434.jpg" alt="" width={300} height={250} />
+      <img className='rounded-2' src={mainLeftBanner} alt="Баннер" width={750} height={270} />
     </div>
        </div>
        <div className='col-6'>
-    555
+    
        </div>
     </div>
   )
