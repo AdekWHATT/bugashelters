1d98b5efb4e2f573055926eb22feb8b54cb7ac12
Vyacheslav K <vyacheslaw.kilin@mail.ru>
2023-03-21 14:33:09 +0300

    stable work

--- src/components/MainInfo/MainInfo.jsx
+++ src/components/MainInfo/MainInfo.jsx
@@ -1,29 +1,29 @@
 import React, { useEffect, useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+// import { useSelector, useDispatch } from 'react-redux';
 import mainLeftBanner from '../../img/1583429434.jpg';
-import { fetchNews } from '../../store/newsSlice';
+// import { fetchNews } from '../../store/newsSlice';
 import './MainInfo.css';
 
 const MainInfo = () => {
-  const dispatch = useDispatch();
-  const rtkNews = useSelector((state) => state.news);
-  const [shouldRender, setShouldRender] = useState(false);
+  // const dispatch = useDispatch();
+  // const rtkNews = useSelector((state) => state.news);
+  // const [shouldRender, setShouldRender] = useState(false);
 
-  useEffect(() => {
-    dispatch(fetchNews());
-  }, [dispatch]);
+  // useEffect(() => {
+  //   dispatch(fetchNews());
+  // }, [dispatch]);
 
-  const { status, news } = rtkNews;
+  // const { status, news } = rtkNews;
 
-  useEffect(() => {
-    if (status === 'succeeded') {
-      setShouldRender(true);
-    }
-  }, [status]);
+  // useEffect(() => {
+  //   if (status === 'succeeded') {
+  //     setShouldRender(true);
+  //   }
+  // }, [status]);
 
-  if (!shouldRender) {
-    return null;
-  }
+  // if (!shouldRender) {
+  //   return null;
+  // }
   // console.log(news.results[0].title);
   return (
     <div className="row p-4">
@@ -33,12 +33,12 @@ const MainInfo = () => {
         </div>
       </div>
       <div className="col-6">
-        <span>Последняя новость:</span>
+        {/* <span>Последняя новость:</span>
         {<><p > {news.results[0].title}</p>
           <a href={news.results[0].link} target="_blank" rel="noopener noreferrer">
             Читать в источнике
           </a></>
-        }
+        } */}
       </div>
     </div>
   );
--- src/components/Weather/Weather.jsx
+++ src/components/Weather/Weather.jsx
@@ -1,33 +1,56 @@
 import { useDispatch, useSelector } from 'react-redux';
-import React, {useEffect } from 'react';
-import { fetchWeatherStart, fetchWeatherSuccess, fetchWeatherFailure , selectWeatherData, selectWeatherLoading, selectWeatherError } from '../../store/weatherSlice';
+import React, { useEffect, useState } from 'react';
+import { fetchWeatherStart, fetchWeatherSuccess, fetchWeatherFailure, selectWeatherData, selectWeatherLoading, selectWeatherError } from '../../store/weatherSlice';
 
 function Weather() {
   const dispatch = useDispatch();
   const weatherData = useSelector(selectWeatherData);
   const weatherLoading = useSelector(selectWeatherLoading);
   const weatherError = useSelector(selectWeatherError);
+  const [city, setCity] = useState('Бугульма');
+
+  const API = 'd683360339220a6f99f853df5f452fc0';
 
   useEffect(() => {
+    // if (city.trim() !== '') { // check if city is not an empty string after trimming whitespace
     dispatch(fetchWeatherStart());
-
-    fetch('http://api.openweathermap.org/data/2.5/onecall?id=524901&appid=d683360339220a6f99f853df5f452fc0')
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=ru&appid=${API}`)
       .then(response => response.json())
       .then(data => dispatch(fetchWeatherSuccess(data)))
       .catch(error => dispatch(fetchWeatherFailure(error)));
-  }, [dispatch]);
+    // }
+  }, [city, dispatch]);
+
+
+  // const handleCityChange = event => {
+  //   setCity(event.target.value);
+  // };
+
+  if (weatherLoading) {
+    return <p>Загрузка...</p>;
+  }
+
+  if (weatherError) {
+    return <p>Ошибка: {weatherError.message}</p>;
+  }
+
+  if (!weatherData) {
+    return null;
+  }
+
+  const celsius = (weatherData.main.temp - 273.15).toFixed(1);
+
   return (
-    <div>
-      {weatherLoading ? (
-        <p>Loading weather data...</p>
-      ) : weatherError ? (
-        <p>Error loading weather data: {weatherError.message}</p>
-      ) : (
-        <>
-          <p>Temperature: {weatherData.list.main.temp}</p>
-          <p>Description: {weatherData.weather[0].description}</p>
-        </>
-      )}
+    <div className='row p-3'>
+      <div className='col-12'>
+        <div className='card_weather'>
+          {/* <h2 className='text-center'>Введите название города:</h2> */}
+          {/* <input type='text' value={city} onChange={handleCityChange} /> */}
+          <h2 className='text-center'>{weatherData.name}</h2>
+          <h1 className='text-center'>Температура: {celsius} °C</h1>
+          <h2 className='text-center'>{weatherData.weather[0].description}</h2>
+        </div>
+      </div>
     </div>
   );
 }
